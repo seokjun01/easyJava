d83754cfa730e412183e7792936211db4f80219e
seokjun01 <suckjungod@naver.com>
2024-08-03 20:41:35 +0900

    init

---
+++ src/ref/NullMain4.java
@@ -0,0 +1,13 @@
+package ref;
+
+public class NullMain4 {
+    public static void main(String[] args) {
+        BigData bigdata = new BigData();
+        bigdata.data = new Data();
+        System.out.println( " bigdata.count =" + bigdata.count); //참조형 초기값 0
+        System.out.println( " bigdata.data =" + bigdata.data); // null로 초기화함
+
+        System.out.println("bigdata.data.value" + bigdata.data.value); //bigdata.data 가 null 임
+    }
+
+}
