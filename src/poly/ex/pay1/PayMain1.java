e24a36c7d41d68c45e9bc6c08f5a26053f432426
seokjun01 <suckjungod@naver.com>
2024-09-15 16:52:26 +0900

    OCP Quiz 2

---
+++ src/poly/ex/pay1/PayMain1.java
@@ -0,0 +1,22 @@
+package poly.ex.pay1;
+
+
+
+public class PayMain1 {
+    //문제 : 리팩토링하기
+    public static void main(String[] args) {
+        PayService payService = new PayService();
+
+        String payOption1 = "kakao";
+        int amount1 = 5000;
+        payService.processPay(payOption1, amount1);
+
+        String payOption2 = "naver";
+        int amount2 = 10000;
+        payService.processPay(payOption2, amount2);
+
+        String payOption3 = "Bad";
+        int amount3 = 15000;
+        payService.processPay(payOption3, amount3);
+    }
+}
