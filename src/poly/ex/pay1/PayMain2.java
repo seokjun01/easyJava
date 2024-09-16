1ff1e7fba629e18de285fac97545fd9cb32bd885
seokjun01 <suckjungod@naver.com>
2024-09-16 19:07:57 +0900

    practice

---
+++ src/poly/ex/pay1/PayMain2.java
@@ -0,0 +1,29 @@
+package poly.ex.pay1;
+
+import java.util.Scanner;
+//기존 결제 시스템이 사용자 입력을 받도록 수정
+
+public class PayMain2 {
+    //문제 : 리팩토링하기
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        PayService payService = new PayService();
+
+        while (true) {
+            System.out.println("결제 수단을 입력하세요 : ");
+            String payOption = scanner.nextLine();
+            if (payOption.equals("exit")) {
+                System.out.println("프로그램을 종료합니다");
+                return;
+            }
+            System.out.println("결제 금액을 입력하세요 : ");
+            int amount = scanner.nextInt();
+            scanner.nextLine();
+
+            payService.processPay(payOption, amount);
+        }
+
+
+    }
+}
+
